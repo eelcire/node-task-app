59d0d56b0816ceac54e607f9d95320b29f7a0bbe
eelcire <eelcire.eelcire@hotmail.com>
2019-10-11 17:20:47 -0500

    1.0.3

--- src/index.js
+++ src/index.js
@@ -4,19 +4,7 @@ const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
-const port = process.env.PORT || 3000
-
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
-
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is currently down. Check back soon!')
-// })
+const port = process.env.PORT
 
 app.use(express.json())
 app.use(userRouter)
